5d915eef9164572d0f553a1eed962fd96ec1ce10
sg3t41 <sg3t41@gmail.com>
2025-05-26 19:21:06 +0900

    first commit

---
+++ src/main/java/com/example/kr_api/PingController.java
@@ -0,0 +1,14 @@
+package com.example.kr_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // このクラスがRESTful APIを扱うコントローラであることを示す
+public class PingController {
+
+    // HTTP GET リクエストが '/ping' パスに送られたときにこのメソッドが実行される
+    @GetMapping("/ping")
+    public String ping() {
+        return "Pong!"; // "Pong!" という文字列をクライアントに返す
+    }
+}
